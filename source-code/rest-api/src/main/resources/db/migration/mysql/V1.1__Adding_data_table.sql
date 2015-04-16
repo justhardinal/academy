8d13943cf89e0347aca4625caf26b4c892467faf
justhardinal <just.hardinal@gmail.com>
2015-04-16 09:32:33 +0700

    add configuration in application.properties and modified dbmigration

--- source-code/rest-api/src/main/resources/db/migration/mysql/V1.1__Adding_data_table.sql
+++ source-code/rest-api/src/main/resources/db/migration/mysql/V1.1__Adding_data_table.sql
@@ -1,2 +1,18 @@
-insert into application_info (ID, NAME) values (1, 'try','trying migrate');
-insert into application_info (ID, NAME) values (2, 'try again','trying to migrate');
+-- MySQL InnoDB Schema
+create table application_info (
+    id VARCHAR(36),
+    info_name VARCHAR(255) NOT NULL,
+    info_description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (info_name)
+)InnoDB;
+
+CREATE TABLE IF NOT EXISTS `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `user` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_petugas`)
+) ENGINE=InnoDB;
+
+insert into application_info (id, info_name, info_description) values ('1', 'try','trying migrate');
+insert into application_info (id, info_name, info_description) values ('2', 'try again','trying to migrate');
