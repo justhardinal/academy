44959b391fd96a1417248b87c0cb49fccfc7c2e2
justhardinal <just.hardinal@gmail.com>
2015-04-16 13:43:46 +0700

    modified db migration

--- source-code/rest-api/src/main/resources/db/migration/mysql/V1.1__Adding_data_table.sql
+++ source-code/rest-api/src/main/resources/db/migration/mysql/V1.1__Adding_data_table.sql
@@ -1,9 +1,9 @@
 ALTER table application_info add update_info varchar(40);
 
-CREATE TABLE IF NOT EXISTS `petugas` (
-  `id_petugas` int(11) NOT NULL,
-  `user` varchar(45) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `officer` (
+  `officerID` int(11) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_petugas`)
+  PRIMARY KEY (`officerID`)
 ) ENGINE=InnoDB;
 
